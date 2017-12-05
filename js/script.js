7a0dc06b3e410a168c003c61e75910219431554c
tabby42 <irene.theiss@gmx.at>
2017-12-05 14:35:04 +0100

    add function for creating table

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Basic 1
 var library = [
 		{ title: 'When Breath Becomes Air', author: 'Paul Kalanithi', readingStatus: true }, 
 		{ title: 'The Girls', author: 'Emma Cline ', readingStatus: true }, 
@@ -19,9 +20,33 @@ function displayReadingStatus ( booklist, id ) {
 
 displayReadingStatus(library, "library");
 
+//Basic 2
 function truncate ( string, numChar ) {
 	var newString = string.substring(0, numChar - 1);
 	return newString;
 }
 
 document.getElementById("truncate").innerHTML = truncate("I am a hero from World of Warcraft", 12);
+
+//Basic 3
+document.getElementById("table-create").addEventListener("click", createTable);
+function createTable () {
+	var rows = Number(prompt("Number of rows: "));
+	var columns = Number(prompt("Number of columns: "));
+	//console.log(rows, columns);
+	var fragment = document.createDocumentFragment();
+	var table = document.createElement("table");
+	var tbody = document.createElement("tbody");
+	for (var i = 0; i < columns; i++) {
+		var column = document.createElement("tr");
+		tbody.appendChild(column);
+		//console.log(rows);
+		for (var k = 0; k < rows; k++) {
+			//console.log(k);
+			column.innerHTML += "<td>Column " + k + "<br>Row " + i + "</td>";
+		}
+	}
+	table.appendChild(tbody);
+	fragment.appendChild(table);
+	document.getElementById("table").appendChild(fragment);
+}
