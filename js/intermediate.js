ef9649eb89c5a4feb64b55f6e7dfc4bd169de094
tabby42 <irene.theiss@gmx.at>
2017-12-05 15:53:19 +0100

    add function for booking seats

--- js/intermediate.js
+++ js/intermediate.js
@@ -1,15 +1,40 @@
 var conference = {
 	availableSeats: 0,
-	alreadyBooked: 0,
-	remainingSeats: function () {
-		return availableSeats - alreadyBooked;
-	}
+	alreadyBooked: 0
 }
 
-document.getElementById("btn-admin").addEventListener("click", processInput);
+function remainingSeats() {
+	return conference.availableSeats - conference.alreadyBooked;
+}
+
+if (document.getElementById("btn-admin")) {
+	document.getElementById("btn-admin").addEventListener("click", processInput);
+}
+if (document.getElementById("btn-user")) {
+	document.getElementById("btn-user").addEventListener("click", bookSeats);
+}
 
 function processInput () {
 	conference.availableSeats = document.getElementById("availSeats").value;
 	conference.alreadyBooked = document.getElementById("booked").value;
-	console.log(conference);
+    localStorage.setItem("conference", JSON.stringify(conference));
+    document.getElementById("confirm").innerHTML = "Your data has been saved!";
+    document.getElementById("availSeats").value = "";
+    document.getElementById("booked").value = "";
+}
+
+function bookSeats () {
+	conference = JSON.parse(localStorage.getItem("conference"));
+	//console.log(conference);
+	var input = Number(document.getElementById("booking").value);
+	if (remainingSeats() >= input) {
+		conference.alreadyBooked = Number(conference.alreadyBooked) + input;
+    	localStorage.setItem("conference", JSON.stringify(conference));
+		document.getElementById("msg").innerHTML =
+		"You have successfully registered " + input + " seats!";
+	} else {
+		document.getElementById("msg").innerHTML =
+		"Sorry, we don't have enough seats left!";
+	}
+	document.getElementById("booking").value = "";
 }
