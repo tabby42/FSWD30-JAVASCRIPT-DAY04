ab206b1c9cbf262e14eb9d3546cce40d3f47f813
tabby42 <irene.theiss@gmx.at>
2017-12-05 11:55:33 +0100

    add js file with script for first basic exercise

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var library = [
+		{ title: 'When Breath Becomes Air', author: 'Paul Kalanithi', readingStatus: true }, 
+		{ title: 'The Girls', author: 'Emma Cline ', readingStatus: true }, 
+		{ title: 'The Nest', author: 'Cynthia', readingStatus: false }, 
+		{ title: 'The Crown', author: 'Kiera Cass', readingStatus: true }, 
+		{ title: 'Behind Closed Doors', author: 'B.A. Paris', readingStatus: false }, 
+		{ title: 'It Ends with Us', author: 'Colleen Hoover', readingStatus: true }
+];
+
+function displayReadingStatus ( booklist, id ) {
+	var list = "";
+	for (var i = 0; i < booklist.length; i++) {
+		list += "<li>" + booklist[i].title + " <em>by " + booklist[i].author + "</em><br><b>" 
+		+ (booklist[i].readingStatus ? ("Already read " +  booklist[i].title ) : ("You still need to read " + booklist[i].title ))
+		+ "</b></li>";
+	}
+	document.getElementById(id).innerHTML = list;
+}
+
+displayReadingStatus(library, "library");
