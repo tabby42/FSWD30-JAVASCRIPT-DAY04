9e5d536e1df017df8caf304dd42cd2834d098865
tabby42 <irene.theiss@gmx.at>
2017-12-07 10:03:55 +0100

    add pi, euler's number and function for displaying last operation

--- challenge/2nd_calculator.js
+++ challenge/2nd_calculator.js
@@ -11,9 +11,11 @@
 			//to which the click event has been attached and not the calculator object
 			try {
 				calculator.result = eval(calculator.input);
-				if ( isFinite(calculator.result) ) {
+				if ( isFinite(calculator.result) && !isNaN(calculator.result) ) {
+					calculator.lastExpr = calculator.input;
 					calculator.input = calculator.result;
 					display.innerHTML = calculator.result;
+					calculator.showLast();
 					return calculator.result;
 				} else {
 					calculator.input = "";
@@ -21,7 +23,6 @@
 					display.innerHTML = "Error";
 					return "Error";
 				}
-				
 			} catch (e) {
 				console.log(e.message);
 				calculator.input = "";
@@ -32,6 +33,7 @@
 		clear: function () {
 			calculator.lastExpr = calculator.input;
 			calculator.input = "";
+			calculator.lastExpr = "";
 			display.innerHTML = "";
 		},
 		sign: function () {
@@ -47,7 +49,6 @@
 			display.innerHTML = calculator.input;
 		},
 		square: function () {
-			//problem: doesn't work properly ...
 			var temp = calculator.calcResult();
 			if (temp !== "Error" && !isNaN(temp)) {
 				calculator.result = temp * temp;
@@ -58,27 +59,49 @@
 				calculator.result = 0;
 				display.innerHTML = "Error";
 			}
+		},
+		pi: function () {
+			calculator.input += Math.PI.toFixed(7);
+			display.innerHTML = calculator.input;
+		},
+		euler: function () {
+			calculator.input += Math.E.toFixed(7);
+			display.innerHTML = calculator.input;
+		},
+		showLast: function () {
+			last.innerHTML = calculator.lastExpr;
+		}
+		squareRoot: function () {
+			
 		}
 	} 
 
 	//variable to store 'screen' of calculator
 	var display = document.getElementById("in");
+	var last = document.getElementById("lastExpression");
 	//Event Handlers
 	var simpleBtns = document.getElementsByClassName("btn");
 	for (var i = 0; i < simpleBtns.length; i++) {
 		simpleBtns[i].addEventListener("click", displayInput);
 	}
+	//equal sign
 	document.getElementById("enter").addEventListener("click", calculator.calcResult);
+	//clear button
 	document.getElementById("clear").addEventListener("click", calculator.clear);
+	//toogle sign
 	document.getElementById("signed").addEventListener("click", calculator.sign);
+	//backdpace
 	document.getElementById("backspace").addEventListener("click", calculator.removeLast);
+	//square
 	document.getElementById("square").addEventListener("click", calculator.square);
-	//decimal point
 	//PI
+	document.getElementById("pi").addEventListener("click", calculator.pi);
 	//e
+	document.getElementById("euler").addEventListener("click", calculator.euler);
 	//percent
 	//square root
-	//
+	document.getElementById("radix").addEventListener("click", calculator.squareRoot);
+
 
 	function displayInput () {
 		calculator.input += this.innerHTML;
